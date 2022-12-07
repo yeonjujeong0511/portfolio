d6bafd9915887b74b32c3b8297123ad9a81cee0e
yeonju0511 <yeonju0511@gmail.com>
2022-12-08 01:34:45 +0900

    section까지 설정

--- portfolio/index.js
+++ portfolio/index.js
@@ -14,3 +14,17 @@ const header = document.querySelector("header");
 const main = document.querySelector("main");
 
 console.dir(main);
+
+for (let i = 0; i < 4; i++) {
+  const section = document.createElement("section");
+  main.append(section);
+}
+
+const sectionClass = ["first-page", "second-page", "third-page", "fourth-page"];
+
+const sections = document.querySelectorAll("section");
+console.log(sections);
+
+sections.forEach((item, index) => {
+  item.className = sectionClass[index];
+});
